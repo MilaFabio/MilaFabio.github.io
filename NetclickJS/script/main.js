eedce6e150703390417b762ef90ccaad616170cb
Mila <milafabio77@gmail.com>
2020-05-26 18:36:34 +0300

    homework-1

--- NetclickJS/script/main.js
+++ NetclickJS/script/main.js
@@ -1,6 +1,8 @@
 //menu
 const leftmenu = document.querySelector(".left-menu");
 const hamburger = document.querySelector(".hamburger");
+const main = document.querySelector('main');
+
 
 // открытие и закрытие меню
 hamburger.addEventListener("click", () => {
@@ -25,3 +27,23 @@ leftmenu.addEventListener("click", (event) => {
         hamburger.classList.add('open');
     }
 });
+
+
+//изменить постер при наведении мышки
+let imageSrc, imageBackdrop;
+main.onmouseover = main.onmouseout = handler;
+
+function handler(event) {
+
+    const target = event.target;
+    const tvCardImg = target.closest('.tv-card__img');
+    if (tvCardImg && event.type == 'mouseover') {
+        imageSrc = tvCardImg.src;
+        imageBackdrop = tvCardImg.dataset.backdrop;
+        tvCardImg.src = imageBackdrop;
+    } else if (tvCardImg && event.type == 'mouseout') {
+        tvCardImg.src = imageSrc;
+        tvCardImg.dataset.backdrop = imageBackdrop;
+
+    }
+}
